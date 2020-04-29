491bcbbfd69ef36fbde953fcbc79b1b745ac6c9e
user42 <pvineet44@gmail.com>
2020-04-29 21:18:07 +0200

    module02 main updated

--- module02/ex01/main.cpp
+++ module02/ex01/main.cpp
@@ -11,8 +11,10 @@ std::cout << "b is " << b << std::endl;
 std::cout << "c is " << c << std::endl;
 std::cout << "d is " << d << std::endl;
 std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+std::cout << "a is " << a.toFloat() << " as float" << std::endl;
 std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+std::cout << "c is " << c.toFloat() << " as float" << std::endl;
 std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 return 0;
 }
